d3b7c93b4053dcad0840e114a4ea0dc015cff172
bibek-in-github <mahat6981@gmail.com>
2023-05-22 11:47:55 +0545

    refactor

--- lib/input_page.dart
+++ lib/input_page.dart
@@ -5,7 +5,10 @@ import 'reusable_card.dart';
 
 const bottomContainerHeight = 80.0;
 const activeCardColor = Color(0xFF1D1E33);
+const inactiveCardColor = Color(0xFF111328);
 const bottomContainerColor = Color(0xFFEB1555);
+
+enum Gender{male, female}
 
 class InputPage extends StatefulWidget {
   @override
@@ -13,6 +16,10 @@ class InputPage extends StatefulWidget {
 }
 
 class _InputPageState extends State<InputPage> {
+
+  Gender selectedGender;
+
+  
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -26,7 +33,12 @@ class _InputPageState extends State<InputPage> {
               children: [
                 Expanded(
                   child: ReusableCard(
-                    colour: activeCardColor,
+                    onPress: (){
+                      setState(() {
+                        selectedGender = Gender.male;
+                      });
+                    },
+                    colour: selectedGender == Gender.male ? activeCardColor : inactiveCardColor,
                     cardChild: IconContent(
                       icon: FontAwesomeIcons.mars,
                       label: 'MALE',
@@ -35,7 +47,12 @@ class _InputPageState extends State<InputPage> {
                 ),
                 Expanded(
                   child: ReusableCard(
-                    colour: activeCardColor,
+                    onPress: (){
+                      setState(() {
+                        selectedGender = Gender.female;
+                      });
+                    },
+                    colour: selectedGender == Gender.female ? activeCardColor : inactiveCardColor,
                     cardChild: IconContent(
                       icon: FontAwesomeIcons.venus,
                       label: 'FEMALE',
@@ -77,6 +94,3 @@ class _InputPageState extends State<InputPage> {
     );
   }
 }
-
-
-
